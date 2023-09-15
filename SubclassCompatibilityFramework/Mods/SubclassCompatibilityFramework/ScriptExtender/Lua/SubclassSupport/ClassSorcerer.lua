9dfc2080c54a912b2fa3529d2b54c30c099ad217
NellsRelo <nellsrelo@gmail.com>
2023-09-15 08:27:05 -0700

    Expose API Cont'd

--- SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/SubclassSupport/ClassSorcerer.lua
+++ SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/SubclassSupport/ClassSorcerer.lua
@@ -5,22 +5,9 @@ local subClasses = {
     subClassGuid = "d007a7be-60c6-4606-87db-66eb9e247050",
     class = "sorcerer",
     subClassName = "Shadow Sorcerery"
-  },
---[[
-  MySubclass = {
-    modGuid = "GUID from your meta.lsx file",
-    suibClassGuid = "UUID from your CLassDescription",
-    class ="sorcerer",
-    subClassName = "Localized Name of Subclass for sorting"
   }
-]] --
 }
 
-if subClasses ~= nil then
-  for _, subClass in pairs(subClasses) do
-    table.insert(Globals.SupportedSubClasses, subClass)
-  end
-end
-
-Globals.SupportedClassDict['sorcerer'] = "42af9bda-9ac0-4564-920b-f9ca1588d6ee"
-Globals.SupportedClassDict['mc_sorcerer'] = "e115c732-80b1-4ae1-bf04-cee44660d64f"
+Api.InsertSubClasses(subClasses)
+Api.InsertClass("sorcerer", "42af9bda-9ac0-4564-920b-f9ca1588d6ee")
+Api.InsertClass("mc_sorcerer", "e115c732-80b1-4ae1-bf04-cee44660d64f")
